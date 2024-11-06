c08aa7e10266566f92b882ffd2a1a7d153c4a0f0
Jake Kincade <jakob.kincade@gmail.com>
2024-11-06 11:44:21 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿/* Name: xxx
+ * Section: CS258-00x
+ * Date:
+ * Program description:
+ */
+using System;
+
+namespace CS258
+{
+    class ShoppingCart
+    {
+        static void Main(string[] args)
+        {
+            // Define variables: item counter and price sum
+
+            // The do-while loop
+            // Textbook@P357
+            do
+            {
+                // Ask user input for the item price
+
+                // Update the current number of items
+                // The number of items determines the shipping cost
+
+
+                // Update the current price sum
+
+                // Check if the user is qualified for free shipping
+                // Textbook@P258, P271 Example 5-8
+                // Prompt user for free shipping if the current price sum is greater than or equal to $90
+
+                // To continue or break a loop
+                // Textbook@P368: break statement, Example 6-18
+                // Textbook@P369: continue statement
+            }
+            while (true);
+
+            // Calculate sales tax, shipping cost etc
+
+            // Print Total purchase charge, number of items purchased,
+            // sales tax amount, shipping charge, and the grand total
+
+        }
+    }
+}
