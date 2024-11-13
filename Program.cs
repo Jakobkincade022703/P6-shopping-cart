fc8ad5c597d1d734bd389804a629c016a584b786
Jake Kincade <jakob.kincade@gmail.com>
2024-11-13 16:03:21 -0600

    finialized code

--- Program.cs
+++ Program.cs
@@ -14,12 +14,13 @@ namespace CS258
         static void Main(string[] args)
         {
             // Define variables: item counter and price sum
-            const double salestax = 0.075;
+            const double salestax = 0.0775;
             int iCount = 0; //users item count
             double iPrice;  // users input price
             double preTotal = 0 ;
             double shipCost;
             string breakOrNot;
+         
             // The do-while loop
             // Textbook@P357
             do
@@ -28,15 +29,27 @@ namespace CS258
                 // Ask user input for the item price
 
                 Console.WriteLine("what is your item price, please ensure price is positive!");
+                Console.WriteLine(" ");
+
                 iPrice = Convert.ToDouble(Console.ReadLine()); //converting users price input
 
                 // Update the current number of items
                 ++iCount;
+                preTotal += iPrice;
                 Console.WriteLine("your current number of items in cart is" + " " + iCount);
+                Console.WriteLine(" ");
+
                 // The number of items determines the shipping cost
+
+                Console.WriteLine("your running total is " + "" + preTotal.ToString("C"));
+                Console.WriteLine(" ");
+
                 if (preTotal >= 100)
                 {
                     shipCost = 0;
+                      Console.WriteLine(" You have earned free shipping!");
+                      Console.WriteLine(" ");
+
                 }
                 else
                 {
@@ -63,33 +76,31 @@ namespace CS258
 
 
                 // Update the current price sum 
-                preTotal = +iPrice;
+            
 
                 // Check if the user is qualified for free shipping
                 if (preTotal > 90 && preTotal < 100)
                 {
-                    Console.WriteLine(" you are only" + " " + (100 - preTotal)+"$" + " " + "away from free shipping!");
-                }
-                else if (preTotal > 100)
-                {
+                    Console.WriteLine(" you are only" + " " + (100 - preTotal).ToString("C") + " " + "away from free shipping!");
+                    Console.WriteLine(" ");
 
-                    Console.WriteLine("you have earned free shipping!");
                 }
-              
+                
 
                 // To continue or break a loop
-                Console.WriteLine("Would you like to continue your shopping experince? please state yes or no");
+                Console.WriteLine("Would you like to continue your shopping experince? please state 'yes' or 'no'. if you state otherwise this will repeat");
+                Console.WriteLine(" ");
                 breakOrNot = Console.ReadLine();
 
                 if (breakOrNot == "no" || breakOrNot == "No")
                 {
-                    continue;
+                    break;
 
                 }
                 else
                     if (breakOrNot == "yes" || breakOrNot == "Yes")
                 {
-                    break;
+                    continue;
                 }
                 else { continue; }
              
@@ -102,14 +113,14 @@ namespace CS258
             double grandTotal = (preTotal * salestax) + preTotal + shipCost;
 
             // Print Total purchase charge, number of items purchased,
-
-            Console.WriteLine("Your total charge is" + " " + grandTotal);
+            Console.WriteLine(" ");
+            Console.WriteLine("Your total charge is" + " " + grandTotal.ToString("C"));
             Console.WriteLine(" ");
             Console.WriteLine("You bought" + " " + iCount + " " + " Items with us today!");
             Console.WriteLine(" ");
-            Console.WriteLine("Your total sales tax is" + " " + (preTotal * salestax));
+            Console.WriteLine("Your total sales tax is" + " " + (preTotal * salestax).ToString("C"));
             Console.WriteLine(" ");
-            Console.WriteLine("Your Shipping fee is" + " " + shipCost);
+            Console.WriteLine("Your Shipping fee is" + " " + shipCost.ToString("C"));
             // sales tax amount, shipping charge, and the grand total
 
         }
